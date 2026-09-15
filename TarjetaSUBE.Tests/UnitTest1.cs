2fae6b48014e8834985920db5b779d04053a8652
n.x <zkolsh@proton.me>
2026-09-15 15:33:33 -0300

    Ejecutable de tests andando tan bien como C#

---
+++ TarjetaSUBE.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+namespace Sube {
+	public class Tests {
+		[SetUp]
+		public void Setup() {
+		}
+
+		[Test]
+		public void Test1() {
+			Assert.Fail("meow meow meow meow meow meow meow meow meow meow meow moew");
+		}
+	}
+}
